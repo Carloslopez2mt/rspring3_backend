c8f0989490997df58a6269f427e8c193b8551cef
Carloslopez2mt <91885968+Carloslopez2mt@users.noreply.github.com>
2021-10-29 10:32:45 -0500

    Update ClienteWeb.java

--- src/main/java/backend/Web/ClienteWeb.java
+++ src/main/java/backend/Web/ClienteWeb.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Client")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ClienteWeb {
        @Autowired
     private ServiciosCliente servicios;
